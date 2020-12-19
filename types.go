e3928a72fcf88d1cd97d6e9569f7e52cbaf0714c
Jon Chen <bsd@voltaire.sh>
2020-12-19 09:11:25 +0000

    add data object

--- types.go
+++ types.go
@@ -1,5 +1,7 @@
 package interactions
 
+import "time"
+
 type InteractionType int
 
 const (
@@ -22,3 +24,37 @@ const (
 type InteractionResponseFlags int64
 
 const Ephemeral InteractionResponseFlags = 1 << 6
+
+type Data struct {
+	Type   InteractionType `json:"type"`
+	Token  string          `json:"token"`
+	Member struct {
+		User struct {
+			ID            int64  `json:"id"`
+			Username      string `json:"username"`
+			Avatar        string `json:"avatar"`
+			Discriminator string `json:"discriminator"`
+			PublicFlags   int64  `json:"public_flags"`
+		} `json:"user"`
+		Roles        []string    `json:"roles"`
+		PremiumSince time.Time   `json:"premium_since"`
+		Permissions  string      `json:"permissions"`
+		Pending      bool        `json:"pending"`
+		Nick         interface{} `json:"nick"`
+		Mute         bool        `json:"mute"`
+		JoinedAt     time.Time   `json:"joined_at"`
+		IsPending    bool        `json:"is_pending"`
+		Deaf         bool        `json:"deaf"`
+	} `json:"member"`
+	ID      string `json:"id"`
+	GuildID string `json:"guild_id"`
+	Data    struct {
+		Options []struct {
+			Name  string `json:"name"`
+			Value string `json:"value"`
+		} `json:"options"`
+		Name string `json:"name"`
+		ID   string `json:"id"`
+	} `json:"data"`
+	ChannelID string `json:"channel_id"`
+}

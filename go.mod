4de5a0c0c408320231fafddcb17860fc03e1d994
Jon Chen <bsd@voltaire.sh>
2020-12-19 18:42:44 +0000

    remove external dependency

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/bsdlp/interactions
 
 go 1.15
-
-require (
-	filippo.io/edwards25519 v1.0.0-beta.2 // indirect
-	github.com/kevinburke/nacl v0.0.0-20201212185859-3ef16edae976
-	golang.org/x/crypto v0.0.0-20201217014255-9d1352758620 // indirect
-)
--- verify.go
+++ verify.go
@@ -6,21 +6,35 @@ import (
 	"crypto/ed25519"
 	"encoding/hex"
 	"io"
+	"io/ioutil"
 	"net/http"
-
-	"github.com/kevinburke/nacl/sign"
+	"strconv"
 )
 
-func Verify(ctx context.Context, r *http.Request, key ed25519.PublicKey) bool {
-	r = r.Clone(ctx)
-	defer r.Body.Close()
+const (
+	publicKeySize = 32
+	signatureSize = 64
+)
 
-	var buf bytes.Buffer
+func verify(sig []byte, key ed25519.PublicKey) bool {
+	if l := len(key); l != publicKeySize {
+		panic("sign: bad public key length: " + strconv.Itoa(l))
+	}
 
-	timestamp := r.Header.Get("X-Signature-Timestamp")
-	if timestamp == "" {
+	if len(sig) < signatureSize || sig[63]&224 != 0 {
 		return false
 	}
+	msg := sig[signatureSize:]
+	sig = sig[:signatureSize]
+
+	return ed25519.Verify(ed25519.PublicKey(key), msg, sig)
+}
+
+// Verify implements the verification side of the discord interactions api
+// signing algorithm, as documented here:
+// https://discord.com/developers/docs/interactions/slash-commands#security-and-authorization
+func Verify(ctx context.Context, r *http.Request, key ed25519.PublicKey) bool {
+	var payloadBuffer bytes.Buffer
 
 	signature := r.Header.Get("X-Signature-Ed25519")
 	if signature == "" {
@@ -31,14 +45,28 @@ func Verify(ctx context.Context, r *http.Request, key ed25519.PublicKey) bool {
 	if err != nil {
 		return false
 	}
+	payloadBuffer.Write(sig)
 
-	buf.Write(sig)
-	buf.WriteString(timestamp)
+	timestamp := r.Header.Get("X-Signature-Timestamp")
+	if timestamp == "" {
+		return false
+	}
 
-	_, err = io.Copy(&buf, r.Body)
+	payloadBuffer.WriteString(timestamp)
+
+	defer r.Body.Close()
+	var body bytes.Buffer
+
+	// at the end of the function, copy the original body back into the request
+	defer func() {
+		r.Body = ioutil.NopCloser(&body)
+	}()
+
+	// copy body into buffers
+	_, err = io.Copy(&payloadBuffer, io.TeeReader(r.Body, &body))
 	if err != nil {
 		return false
 	}
 
-	return sign.PublicKey(key).Verify(buf.Bytes())
+	return verify(payloadBuffer.Bytes(), key)
 }

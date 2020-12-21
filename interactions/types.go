3dfad4c32da836d7cf1b07745638fd3b344e568d
Jon Chen <bsd@voltaire.sh>
2020-12-21 04:59:19 +0000

    fix data types

--- interactions/types.go
+++ interactions/types.go
@@ -30,21 +30,21 @@ type Data struct {
 	Token  string          `json:"token"`
 	Member struct {
 		User struct {
-			ID            int64  `json:"id"`
+			ID            string `json:"id"`
 			Username      string `json:"username"`
 			Avatar        string `json:"avatar"`
 			Discriminator string `json:"discriminator"`
 			PublicFlags   int64  `json:"public_flags"`
 		} `json:"user"`
-		Roles        []string    `json:"roles"`
-		PremiumSince time.Time   `json:"premium_since"`
-		Permissions  string      `json:"permissions"`
-		Pending      bool        `json:"pending"`
-		Nick         interface{} `json:"nick"`
-		Mute         bool        `json:"mute"`
-		JoinedAt     time.Time   `json:"joined_at"`
-		IsPending    bool        `json:"is_pending"`
-		Deaf         bool        `json:"deaf"`
+		Roles        []string  `json:"roles"`
+		PremiumSince time.Time `json:"premium_since"`
+		Permissions  string    `json:"permissions"`
+		Pending      bool      `json:"pending"`
+		Nick         string    `json:"nick"`
+		Mute         bool      `json:"mute"`
+		JoinedAt     time.Time `json:"joined_at"`
+		IsPending    bool      `json:"is_pending"`
+		Deaf         bool      `json:"deaf"`
 	} `json:"member"`
 	ID      string `json:"id"`
 	GuildID string `json:"guild_id"`
